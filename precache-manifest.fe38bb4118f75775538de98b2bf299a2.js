bc85ef52ecff164633b762992e26ca66e9de20af
unknown <oghenekaro57@gmail.com>
2020-05-29 01:47:41 +0100

    Updates

---
+++ precache-manifest.fe38bb4118f75775538de98b2bf299a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8b89074e74f9493a716df80b817350",
+    "url": "/Monster-Application/index.html"
+  },
+  {
+    "revision": "094e4cc99a4a112a3557",
+    "url": "/Monster-Application/static/css/main.3a05f63a.chunk.css"
+  },
+  {
+    "revision": "b4e45ef4649f22162251",
+    "url": "/Monster-Application/static/js/2.e1327f51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Application/static/js/2.e1327f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094e4cc99a4a112a3557",
+    "url": "/Monster-Application/static/js/main.db5b8931.chunk.js"
+  },
+  {
+    "revision": "5fa4be5a5bb6b243df95",
+    "url": "/Monster-Application/static/js/runtime-main.d6a6a89b.js"
+  }
+]);
